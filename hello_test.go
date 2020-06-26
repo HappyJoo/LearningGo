a0e72ed22812ee1ae693a7e61baa200dd507d9fd
happyjoo <happy_joo@163.com>
2020-06-26 17:27:16 +0800

    test

---
+++ hello_test.go
@@ -0,0 +1,30 @@
+package main
+import "testing"
+
+// func Hello(name string) string {
+// 	return "Hello, world"
+// }
+
+func TestHello(t *testing.T) {
+
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	t.Run("Saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+}
