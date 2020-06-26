b5a3802aa517dedc2612552509dd84fffd107c77
happyjoo <happy_joo@163.com>
2020-06-26 17:58:04 +0800

    Rename file

--- hello_test.go
+++ hello_test.go
@@ -15,15 +15,26 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("Saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 
-	t.Run("Say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
-		want := "Hello, World"
+	t.Run("In Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
 
+	t.Run("In French", func(t *testing.T) {
+		got := Hello("Frank", "French")
+		want := "Bonjour, Frank"
+		assertCorrectMessage(t, got, want)
+	})
+	
+	t.Run("Say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
 		assertCorrectMessage(t, got, want)
 	})
 
---
+++ note.md
@@ -0,0 +1,5 @@
+Writing a test is just like writing a function, with a few rules
+
+- It needs to be in a file with a name like xxx_test.go
+- The test function must start with the word Test
+- The test function takes one argument only t *testing.T
