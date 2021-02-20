ccd8851dfc0e05d945aee6d87da4d14cdec26222
happyjoo <happy_joo@163.com>
2021-02-20 17:20:08 +0800

    update

--- dependency-injection/di.go
+++ dependency-injection/di.go
@@ -1,19 +1,5 @@
 package main
 
-// import (
-// 	"fmt"
-// 	"os"
-// 	"io"
-// )
-
-// func Greet(writer io.Writer, name string) {
-// 	fmt.Fprintf(writer, "Hello, %s", name)
-// }
-
-// func main() {
-// 	Greet(os.Stdout, "Elodie")
-// }
-
 import (
 	"fmt"
 	"io"
