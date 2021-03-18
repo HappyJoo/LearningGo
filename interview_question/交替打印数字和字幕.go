2418f8512b964d0eabb7989e9bba03867b644f96
happyjoo <Happy_Joo@163.com>
2021-03-18 17:24:40 +0800

    Update 交替打印数字和字幕.go

--- interview_question/交替打印数字和字幕.go
+++ interview_question/交替打印数字和字幕.go
@@ -20,7 +20,7 @@ func main() {
 				i++
 				fmt.Print(i)
 				i++
-				letter <- true
+				letter <- true // call letter func
 				break
 			default:
 				break
@@ -31,23 +31,23 @@ func main() {
 	wait.Add(1)
 	go func(wait *sync.WaitGroup) {
 		str := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
+		strLength := strings.Count(str, "") - 1
 		i := 0
 		for {
 			select {
 			case <- letter:
-				if i >= strings.Count(str, "") - 1{
+				if i >= strLength {
 					wait.Done()
 					return 
 				}
 				fmt.Print(str[i:i+1])
 				i++
-				if i >= strings.Count(str, "") {
+				if i >= strLength {
 					i = 0
 				}
 				fmt.Print(str[i:i+1])
 				i++
-				number <- true
+				number <- true // call number func
 				break
 			default:
 				break
@@ -66,16 +66,16 @@ import (
 )
 
 func main() {
-	chanNumber := make(chan bool)
-	chanLetter := make(chan bool, 1)
+	chanNumber := make(chan bool, 1)
+	chanLetter := make(chan bool)
 	done := make(chan int)
 
 	go func() {
 		for i := 1; i < 26; i += 2 {
-			<-chanLetter 
+			<-chanNumber 
 			fmt.Print(i)
 			fmt.Print(i + 1)
-			chanNumber <- true
+			chanLetter <- true // chanLetter in to call letter func
 		}
 	}()
 
@@ -83,15 +83,18 @@ func main() {
 		letters := []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", 
 		"L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
 		for i := 0; i < 26; i += 2 {
-			<-chanNumber 
+			<-chanLetter 
 			fmt.Print(letters [i])
 			fmt.Print(letters [i+1])
-			chanLetter <- true
+			chanNumber <- true // chanNumber in to call number func
 		}
 		done <- 1
 	}()
-
-	chanLetter <- true
+	
+	// chanNumber 传入之后才可以触发 number func
+	chanNumber<- true
+	// 全部跑完之后 done <- 1 然后触发 done
 	<-done
+
 }
 
